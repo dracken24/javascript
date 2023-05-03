b6c5cc1b9f203a6fa8365f215045c93b6d45035b
dracken24 <dracken24@gmail.com>
2023-05-02 21:49:36 -0400

    ok

---
+++ ex00/pop-up.js
@@ -0,0 +1,22 @@
+// 'let' more use than 'var'
+// let mor for local and var more for global variable
+
+// for (let i = 0; i < 5; i++)
+// {
+//     console.log(i);
+// }
+
+alert("Don't click on that button.");
+alert("Really !!?!, what dou you think when i say dont click on that button?");
+alert("DON'T F*#@&?! CLICK AGAIN ON THAT F@?*$%& BUTTON!!!!!!!!");
+
+let ret = confirm("Go f*#$ yourselve, did i give up??");
+
+if (ret == true)
+	alert("You hit ok");
+if (ret == false)
+	alert("You hit cancel");
+
+let str = prompt("Enter your name.");
+alert("Hi " + str);
+console.log(str, "is just a son of a b#@%?")
