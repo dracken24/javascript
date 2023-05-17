6066ce6146cf7ad92e3353e0d6a800fa6d236b12
dracken24 <dracken24@gmail.com>
2023-05-16 20:37:02 -0400

    work

--- graph/core/src/index.js
+++ graph/core/src/index.js
@@ -32,19 +32,21 @@ class HitPoint {
 }
 
 class Player {
-	constructor(hit, imgLink, obj) {
+	constructor(hit, imgLink, obj, hitZone) {
 		this.hitPoint = hit;
 		this.imgLink = imgLink;
 		this.obj = obj;
+		this.hitZone = hitZone;
 	}
 }
 
 //--------------------------------------------- Vars -------------------------------------------------//
 
-let p1 = new Player([0, 0], player1, 0);
-let p2 = new Player([0, 0], player2, 0);
+let p1 = new Player([0, 0], player1, 0, [0, 0, 0, 0, 0]);
+let p2 = new Player([0, 0], player2, 0, [0, 0, 0, 0, 0]);
 
-let ball = new Player([0, 0], player2, 0);
+let ball = new Player([0, 0], player2, 0, [0, 0, 0, 0, 0]);
+let ballAngleModifier = 0;
 
 let ctUpP1 = 0;
 let ctDownP1 = 0;
@@ -52,6 +54,7 @@ let ctUpP2 = 0;
 let ctDownP2 = 0;
 
 let ctBall = -1;
+let ctBallBound = -1;
 
 //----------------------------------------- catch key events ------------------------------------------//
 // Add event listener on keydown
@@ -107,8 +110,9 @@ class MyGame extends Phaser.Scene
 		ControlP2();
 
 		BallControl();
-
+		
 		CheckCollision();
+		SavePositions();
 	}
 }
 
@@ -133,7 +137,7 @@ const game = new Phaser.Game(config);
 
 function MovePlayerDown(event) {
 	const keyPressed = event.key;
-	console.log("You hit Down: " + keyPressed);
+	// console.log("You hit Down: " + keyPressed);
 
 	// P1 up and down ct
 	if (keyPressed === 'o') {
@@ -160,7 +164,7 @@ function MovePlayerDown(event) {
 
 function MovePlayerUp(event) {
 	const keyPressed = event.key;
-	console.log("You hit Up: " + keyPressed);
+	// console.log("You hit Up: " + keyPressed);
 
 	// P1 up and down
 	if (keyPressed === 'o') {
@@ -182,10 +186,26 @@ function MovePlayerUp(event) {
 function BallControl() {
 	if (ctBall == 1) {
 		ball.obj.x -= 2;
+		ball.obj.y += ballAngleModifier;
 	}
 	else if (ctBall == -1) {
 		ball.obj.x += 2;
+		ball.obj.y += ballAngleModifier;
 	}
+
+	if (ball.hitZone.y1 <= 5)
+	{
+		ballAngleModifier *= -1;
+		ball.obj.y = 24;
+	}
+	else if (ball.hitZone.y2 >= 795)
+	{
+		ballAngleModifier *= -1;
+		ball.obj.y = 776;
+	}
+	// console.log("rebound 1: " + ball.hitZone.y1);
+	// console.log("rebound 2: " + ball.hitZone.y2);
+	// console.log("ballAngleModifier: " + ballAngleModifier);
 }
 
 function ControlP1() {
@@ -208,40 +228,87 @@ function ControlP2() {
 	}
 }
 
+function SavePositions() {
+	p1.hitZone.x1 = p1.obj.x - (p1.obj.width / 2);
+	p1.hitZone.x2 = p1.obj.x + (p1.obj.width / 2);
+	p1.hitZone.y1 = p1.obj.y - (p1.obj.height / 2);
+	p1.hitZone.y2 = p1.obj.y + (p1.obj.height / 2);
+	p1.hitZone.moyY = p1.hitZone.y1 + (p1.obj.height / 2);
+
+	p2.hitZone.x1 = p2.obj.x - (p2.obj.width / 2);
+	p2.hitZone.x2 = p2.obj.x + (p2.obj.width / 2);
+	p2.hitZone.y1 = p2.obj.y - (p2.obj.height / 2);
+	p2.hitZone.y2 = p2.obj.y + (p2.obj.height / 2);
+	p2.hitZone.moyY = p2.hitZone.y1 + (p2.obj.height / 2);
+
+	ball.hitZone.x1 = ball.obj.x - (ball.obj.width / 2);
+	ball.hitZone.x2 = ball.obj.x + (ball.obj.width / 2);
+	ball.hitZone.y1 = ball.obj.y - (ball.obj.height / 2);
+	ball.hitZone.y2 = ball.obj.y + (ball.obj.height / 2);
+	ball.hitZone.moyY = ball.hitZone.y1 + (ball.obj.height / 2);
+}
+
 function CheckCollision() {
+	
+
 	if (Phaser.Geom.Intersects.RectangleToRectangle(ball.obj.getBounds(), p1.obj.getBounds())) {
 		// Collision detected player1 and ball
-		
-		p1.hitPoint = Phaser.Geom.Intersects.GetRectangleToRectangle(p1.obj.getBounds(), ball.obj.getBounds());
-		// p1.hitPoint.y = Phaser.Geom.Intersects.GetRectangleToRectangle(p1.obj.getBounds(), ball.obj.getBounds()).y + ball.obj.height / 2;
-		// console.log(hit.y);
-		console.log("hitx 0: " + p1.hitPoint[0].x);
-		console.log("hity 0: " + p1.hitPoint[0].y);
-		console.log("hitx 1: " + p1.hitPoint[1].x);
-		console.log("hity 1: " + p1.hitPoint[1].y);
-		// console.log(Phaser.Geom.Intersects.GetRectangleIntersection(ball.getBounds(), p1.getBounds()));
 
-		
-		console.log('Collision player1 and ball');
+		// console.log("img.x1 : " + p1.hitZone.x1);
+		// console.log("img.x2 : " + p1.hitZone.x2);
+		// console.log("img.y1 : " + p1.hitZone.y1);
+		// console.log("img.y2 : " + p1.hitZone.y2);
+		// console.log("p1.moy   : " + p1.hitZone.moyY);
+		// console.log("ball.moy : " + ball.hitZone.moyY);
+		console.log("ballAngleModifier : " + ballAngleModifier);
+
+		// console.log('Collision player1 and ball');
 		if (ctBall == -1)
 		{
-			ctBall = ctBall * -1;
+			if (ball.hitZone.moyY > p1.hitZone.moyY)
+			{
+				ballAngleModifier += 0.3;
+			}
+			else if (ball.hitZone.moyY < p1.hitZone.moyY)
+			{
+				ballAngleModifier -= 0.3;
+			}
+			else
+			{
+				ballAngleModifier = 0;
+			}
+
 			if (ctUpP1 == 1)
 			{
 				// TODO: move in y
 			}
+			ctBall = ctBall * -1;
 		}
 	}
 	else if (Phaser.Geom.Intersects.RectangleToRectangle(p2.obj.getBounds(), ball.obj.getBounds())) {
 		// Collision detected player2 and ball
 		console.log('Collision player2 and ball');
+
 		if (ctBall == 1)
 		{
-			ctBall = ctBall * -1;
+			if (ball.hitZone.moyY > p2.hitZone.moyY)
+			{
+				ballAngleModifier += 0.3;
+			}
+			else if (ball.hitZone.moyY < p2.hitZone.moyY)
+			{
+				ballAngleModifier -= 0.3;
+			}
+			else
+			{
+				ballAngleModifier = 0;
+			}
+
 			if (ctUpP1 == 1)
 			{
 				// TODO: move in y
 			}
+			ctBall = ctBall * -1;
 		}
 	}
 
@@ -251,11 +318,15 @@ function CheckCollision() {
 		// console.log('Player2 Win this round');
 		ctBall = ctBall * -1;
 		ball.obj.x = 600;
+		ball.obj.y = 400;
+		ballAngleModifier = 0;
 	}
 	else if (ball.obj.x < -100)
 	{
 		// console.log('Player1 Win this round');
 		ctBall = ctBall * -1;
 		ball.obj.x = 600;
+		ball.obj.y = 400;
+		ballAngleModifier = 0;
 	}
 }
